ff50b67a6e223fe9a282aeca38127fc533cfc157
THIRTEEN <1481031171@qq.com>
2025-04-07 20:48:53 +0800

    Change file -main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Git!" << std::endl;
     return 0;
 }
 // This is a simple C++ program that prints "Hello, World!" to the console.
